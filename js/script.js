c64ffc121cddde49445e4cb151e4cc3571cdd7ee
unknown <alstrekozov@STD4.arteast.local>
2019-12-05 17:25:27 +0500

    Правки #1

--- js/script.js
+++ js/script.js
@@ -9,8 +9,22 @@
           return false;
       });
     }
+    function clickImg(idImg) {
+      var anchor = $(this);
+      $(idImg).on("click", function(){
+        if($(idImg).css("position") == "absolute") {
+          $(idImg).removeClass("positionAbsolute");
+        } else {
+            $(idImg).addClass("positionAbsolute");
+        }
+      })
+    }
     scroll("#btnQuestions");
     scroll("#btnParticipation");
     scroll("#btnExchange");
     scroll("#btnRegulations");
+    scroll("#btnProd");
+    clickImg("#icon1");
+    clickImg("#icon2");
+    clickImg("#icon3");
   });
